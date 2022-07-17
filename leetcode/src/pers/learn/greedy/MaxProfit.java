4b59ee82985c23d8c5ef0fcaa45026e0f4805944
hao <tanaho@qq.com>
2022-07-17 21:24:50 +0800

    122. 买卖股票的最佳时机 II

--- leetcode/src/pers/learn/greedy/MaxProfit.java
+++ leetcode/src/pers/learn/greedy/MaxProfit.java
@@ -1,9 +1,15 @@
 package pers.learn.greedy;
 
-/**
- * 121. 买卖股票的最佳时机
- */
 public class MaxProfit {
+    /**
+     * 121. 买卖股票的最佳时机
+     * 给定一个数组 prices ，它的第i 个元素prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     *
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
@@ -12,6 +18,25 @@ public class MaxProfit {
                 minPrice = prices[i];
             } else if (prices[i] - minPrice > maxProfit) {
                 maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    /**
+     * 122. 买卖股票的最佳时机 II
+     * 给你一个整数数组 prices ，其中prices[i] 表示某支股票第 i 天的价格。
+     * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候最多只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+     * 返回 你能获得的 最大 利润。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i]) {
+                maxProfit += (prices[i + 1] - prices[i]);
             }
         }
         return maxProfit;
