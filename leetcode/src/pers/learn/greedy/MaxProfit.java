2bab9275dbb7fdf90f8f573a7c2d2fb371dda8bc
hao <tanaho@qq.com>
2022-07-17 20:55:17 +0800

    121. 买卖股票的最佳时机

---
+++ leetcode/src/pers/learn/greedy/MaxProfit.java
@@ -0,0 +1,19 @@
+package pers.learn.greedy;
+
+/**
+ * 121. 买卖股票的最佳时机
+ */
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (minPrice > prices[i]) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
