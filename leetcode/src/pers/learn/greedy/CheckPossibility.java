00664593e8e3b6937d41d374b4b4b590c106f68e
hao <tanaho@qq.com>
2022-08-15 20:52:43 +0800

    665. 非递减数列

--- leetcode/src/pers/learn/greedy/CheckPossibility.java
+++ leetcode/src/pers/learn/greedy/CheckPossibility.java
@@ -2,14 +2,16 @@ package pers.learn.greedy;
 
 public class CheckPossibility {
     public boolean checkPossibility(int[] nums) {
-        int max = nums[0];
+        int length = nums.length;
         int cnt = 0;
-        for (int num : nums) {
-            if (num >= max) {
-                max = num;
-            } else {
+        for (int i = 0; i < length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                if (i > 0 && nums[i + 1] >= nums[i - 1]) {
+                    nums[i] = nums[i - 1];
+                } else if (i > 0 && nums[i + 1] < nums[i - 1]) {
+                    nums[i + 1] = nums[i];
+                }
                 cnt++;
-                max = num;
             }
         }
         return cnt < 2;
@@ -17,6 +19,9 @@ public class CheckPossibility {
 
     public static void main(String[] args) {
         CheckPossibility checkPossibility = new CheckPossibility();
-        checkPossibility.checkPossibility(new int[]{3,4,2,3});
+        checkPossibility.checkPossibility(new int[]{4, 2, 1});
+        //4, 2, 3
+        //1, 4, 2, 3
+        //3, 4, 2, 3
     }
 }
