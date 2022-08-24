3dbd5fa2829d1861a8df63f30608885e4ac5ee92
hao <tanaho@qq.com>
2022-08-24 11:47:26 +0800

    69. x 的平方根

---
+++ leetcode/src/pers/learn/binarySearch/Sqrtx.java
@@ -0,0 +1,28 @@
+package pers.learn.binarySearch;
+
+public class Sqrtx {
+    /**
+     * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+     * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+     *
+     * @param x
+     * @return
+     */
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        int left = 0;
+        int right = x / 2;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (mid > x / mid) {
+                right = mid - 1;
+            }
+            else {
+                left = mid;
+            }
+        }
+        return left;
+    }
+}
