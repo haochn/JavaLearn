4a2e023388e1d26ecf36df650e408235f2c4d226
hao <tanaho@qq.com>
2022-11-22 14:33:16 +0800

    reflect

--- jarTest/src/test/java/pers/learn/UtilsTest.java
+++ jarTest/src/test/java/pers/learn/UtilsTest.java
@@ -5,8 +5,32 @@ import org.junit.Test;
 import pers.learn.utils.Opencsv;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class UtilsTest {
+
+    public void test() {
+        try {
+            Class<?> portSynClass = Class.forName("com.im.ap.defactopost.syn.DefactoPortSynInventery");
+            Object portSynClassInstance = portSynClass.newInstance();
+            Method clearCurMaxSeqMap = portSynClass.getMethod("clearCurMaxSeqMap");
+            clearCurMaxSeqMap.invoke(portSynClassInstance);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
     @Test
     public void testCsv() throws CsvValidationException, IOException {
         Opencsv opencsv = new Opencsv();
