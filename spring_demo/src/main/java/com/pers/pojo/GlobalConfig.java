98afcf288af85dee9327b216cc15469025ce8a21
Hao <tanaho@qq.com>
2023-06-27 19:10:57 +0800

    update Spring IoC 2.1

--- spring_demo/src/main/java/com/pers/pojo/GlobalConfig.java
+++ spring_demo/src/main/java/com/pers/pojo/GlobalConfig.java
@@ -15,7 +15,7 @@ public class GlobalConfig {
     private String path;
     // @Autowired 通过 类型DataConfig.class 从IoC中取
     @Autowired
-    // @Qualifier("config") 通过name = "config" 去IoC容器找对象
+    // @Qualifier("config") 通过name = "config" 去IoC容器找对象,那么DataConfig类的注解应该是@Component("config")
     @Qualifier("config")
     private DataConfig dataConfig;
 }
