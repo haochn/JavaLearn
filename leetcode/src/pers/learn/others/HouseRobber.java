fab66f00adc8b3672118e16e2f75b4b452186f59
Hao <tanaho@qq.com>
2023-09-22 14:44:43 +0800

    213. 打家劫舍 II

--- leetcode/src/pers/learn/others/HouseRobber.java
+++ leetcode/src/pers/learn/others/HouseRobber.java
@@ -1,5 +1,7 @@
 package pers.learn.others;
 
+import java.util.Arrays;
+
 public class HouseRobber {
     /**
      * 198. 打家劫舍
@@ -23,6 +25,43 @@ public class HouseRobber {
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
 
+        return dp[n];
+    }
+
+
+    /**
+     * 213. 打家劫舍 II
+     * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+     * <p>
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+     * 输入：nums = [2,3,2]
+     * 输出：3
+     * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+     */
+    public int rob2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] tmp = Arrays.copyOfRange(nums, 0, nums.length - 1);
+        int[] tmp2 = Arrays.copyOfRange(nums, 1, nums.length);
+        return Math.max(robHelper(tmp2), robHelper(tmp));
+    }
+
+    private int robHelper(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
         return dp[n];
     }
 }
