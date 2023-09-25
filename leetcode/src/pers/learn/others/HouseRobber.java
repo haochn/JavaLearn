1577c36debfd6793c5b549203b308d4c0001cb3d
Hao <tanaho@qq.com>
2023-09-25 14:58:37 +0800

    337. 打家劫舍 III

--- leetcode/src/pers/learn/others/HouseRobber.java
+++ leetcode/src/pers/learn/others/HouseRobber.java
@@ -1,6 +1,8 @@
 package pers.learn.others;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HouseRobber {
     /**
@@ -64,4 +66,94 @@ public class HouseRobber {
         }
         return dp[n];
     }
+
+    /**
+     * 337. 打家劫舍 III
+     * 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+     * 除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+     * 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+     * 输入: root = [3,2,3,null,3,null,1]
+     * 输出: 7
+     * 解释: 小偷一晚能够盗取的最高金额 3 + 3 + 1 = 7
+     */
+    /**
+     * Definition for a binary tree node.
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 递归
+    public int rob3_1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int money = root.val;
+        if (root.left != null) {
+            money += rob3_1(root.left.left) + rob3_1(root.left.right);
+        }
+        if (root.right != null) {
+            money += rob3_1(root.right.left) + rob3_1(root.right.right);
+        }
+        return Math.max(money, rob3_1(root.left) + rob3_1(root.right));
+    }
+
+    // 递归 + 使用map记忆化
+    public int rob3_2(TreeNode root) {
+        HashMap<TreeNode, Integer> map = new HashMap<>(16);
+        return rob3_2Helper(root, map);
+    }
+
+    public int rob3_2Helper(TreeNode root, Map map) {
+        if (root == null) {
+            return 0;
+        }
+        if (map.containsKey(root)) {
+            return (Integer) map.get(root);
+        }
+        int money = root.val;
+        if (root.left != null) {
+            money += rob3_2Helper(root.left.left, map) + rob3_2Helper(root.left.right, map);
+        }
+        if (root.right != null) {
+            money += rob3_2Helper(root.right.left, map) + rob3_2Helper(root.right.right, map);
+        }
+        int max = Math.max(money, rob3_2Helper(root.left, map) + rob3_2Helper(root.right, map));
+        map.put(root, max);
+        return max;
+    }
+
+    public int rob3_3(TreeNode root) {
+        int[] result = rob3_3Helper(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    private int[] rob3_3Helper(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] left = rob3_3Helper(root.left);
+        int[] right = rob3_3Helper(root.right);
+
+        int[] result = new int[2];
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = root.val + left[0] + right[0];
+        return result;
+    }
+
 }
