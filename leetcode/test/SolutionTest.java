5652a207f43a77549b6c7929cf7ba10701c4c7d5
Hao <tanaho@qq.com>
2023-12-29 18:03:09 +0800

    2706. 购买两块巧克力

--- leetcode/test/SolutionTest.java
+++ leetcode/test/SolutionTest.java
@@ -8,4 +8,11 @@ public class SolutionTest {
         int[][] roads = new int[][]{{3, 1}, {3, 2}, {1, 0}, {0, 4}, {0, 5}, {4, 6}};
         solution.minimumFuelCost(roads, 2);
     }
+
+    @Test
+    public void buyChocoTest() {
+        Solution solution = new Solution();
+        int[] prices = new int[]{3, 2, 3};
+        solution.buyChoco(prices, 3);
+    }
 }
