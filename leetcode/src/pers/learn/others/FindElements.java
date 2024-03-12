a10623262c275852362142c90fba87abbf05ff69
Hao <tanaho@qq.com>
2024-03-12 11:23:59 +0800

    1261. 在受污染的二叉树中查找元素

---
+++ leetcode/src/pers/learn/others/FindElements.java
@@ -0,0 +1,60 @@
+package pers.learn.others;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * 1261. 在受污染的二叉树中查找元素
+ * 给出一个满足下述规则的二叉树：
+ * root.val == 0
+ * 如果 treeNode.val == x 且 treeNode.left != null，那么 treeNode.left.val == 2 * x + 1
+ * 如果 treeNode.val == x 且 treeNode.right != null，那么 treeNode.right.val == 2 * x + 2
+ * 现在这个二叉树受到「污染」，所有的 treeNode.val 都变成了 -1。
+ * 请你先还原二叉树，然后实现 FindElements 类：
+ * FindElements(TreeNode* root) 用受污染的二叉树初始化对象，你需要先把它还原。
+ * bool find(int target) 判断目标值 target 是否存在于还原后的二叉树中并返回结果。
+ */
+
+public class FindElements {
+    private Set<Integer> set = new HashSet<>();
+
+    public FindElements(TreeNode root) {
+        dfs(root, 0);
+    }
+
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+
+    private void dfs(TreeNode root, int val) {
+        if (root == null) {
+            return;
+        }
+        set.add(val);
+        dfs(root.left, 2 * val + 1);
+        dfs(root.right, 2 * val + 2);
+    }
+}
